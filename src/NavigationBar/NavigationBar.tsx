8ecacd68274495d190aaba5c98ed75ba6d42e04f
“ZiyedB” <zdiyez+githubjob@gmail.com>
2021-02-10 18:04:25 +0000

    feat(navigation-bar): stylise navigation bar similar to site

---
+++ src/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FaUserCircle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+import './NavigationBar.scss';
+
+export const NavigationBar = (): JSX.Element => {
+  return (
+    <div className="navigation-bar">
+      <nav className="navigation-bar__nav">
+        <div className="navigation-bar__general">
+          <Link className="navigation-bar__logo" to="/">
+            <img className="navigation-bar__logo-img" src="/assets/images/logo-icon.png" alt="Paralink Network" />
+          </Link>
+          <div className="navigation-bar__content">
+            <ul className="navigation-bar__links">
+              <li className="navigation-bar__link">
+                <Link to="/ipfs">Paralink Network</Link>
+              </li>
+              <li className="navigation-bar__link">
+                <Link to="/ipfs">IPFS</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="navigation-bar__user">
+          {/* TODO: Need to be a user page */}
+          <a className="navigation-bar__link navigation-bar__user-link" href="/">
+            <FaUserCircle />
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavigationBar;
--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,7 +1,6 @@
-import { Container, Menu } from 'semantic-ui-react';
-
-import { Link } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
 import React, { ReactNode } from 'react';
+import { NavigationBar } from '../NavigationBar';
 
 interface BaseLayoutProps {
   children: any;
@@ -10,16 +9,7 @@ interface BaseLayoutProps {
 function BaseLayout(props: BaseLayoutProps): ReactNode {
   return (
     <div>
-      <Menu fixed="top" inverted>
-        <Container>
-          <Menu.Item header>
-            <Link to="/ipfs">Paralink Network</Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link to="/ipfs">IPFS</Link>
-          </Menu.Item>
-        </Container>
-      </Menu>
+      <NavigationBar />
 
       <Container textAlign="left" style={{ marginTop: '7em' }}>
         {props.children}
