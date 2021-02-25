e7be84ca180cbcbd988ae69538df67038b0b69e3
Žiga Franko <franko.ziga@gmail.com>
2021-02-25 18:20:32 +0100

    IPFS and IPFSList components redefined

---
+++ src/components/pages/ipfs/Ipfs.tsx
@@ -0,0 +1,94 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Button, Grid, Message } from 'semantic-ui-react';
+
+import ace from 'brace';
+import 'brace/mode/json';
+import 'brace/theme/github';
+
+// No typings for jsoneditor-react for now just using @ts-ignore for getting around the issue
+// Later on with advanced design we might not used that library. If we do we can as well define typings for it
+// @ts-ignore
+import { JsonEditor as Editor } from 'jsoneditor-react';
+import 'jsoneditor-react/es/editor.min.css';
+import { LoadPQLHash } from './pql';
+import Error from './Error';
+import { axiosInstance } from '../../../api/api';
+
+interface PqlAction {
+  success: string;
+}
+
+const pqlAction = (endpoint: string, text: LoadPQLHash, setResult: (value: string) => void): void => {
+  Promise.resolve()
+    .then(() => axiosInstance.post<PqlAction>(`${endpoint}`, { text }))
+    .then((res) => res.data.success)
+    .then(setResult)
+    .catch((err) => setResult(err.message));
+};
+
+interface UrlParams {
+  hash: string;
+}
+
+const Ipfs: React.FC<{}> = () => {
+  const { hash } = useParams<UrlParams>();
+  const editor = useRef<Editor>();
+
+  const [error, setError] = useState('');
+  const [resultContent, setResultContent] = useState('');
+
+  const getContent = (): LoadPQLHash => editor.current.jsonEditor.getText();
+  const testAction = (): void => pqlAction('ipfs/test', getContent(), setResultContent);
+  const saveAction = (): void => pqlAction('ipfs/save_pql', getContent(), setResultContent);
+
+  useEffect(() => {
+    Promise.resolve()
+      .then(() => axiosInstance.get<LoadPQLHash>(`/ipfs/${hash}`))
+      .then((res) => res.data)
+      .then((res) => {
+        editor.current.jsonEditor.set(res.pql);
+        setResultContent(res.hash);
+      })
+      .catch((err) => setError(err.message));
+  }, [hash]);
+
+  return error.length > 0 ? (
+    <Error message={error} hash={hash} />
+  ) : (
+    <Grid>
+      <Grid.Row>
+        <div className="ui big breadcrumb">
+          <Link to="/ipfs">IPFS</Link>
+          <i className="right chevron icon divider" />
+          <div className="active section">{hash}</div>
+        </div>
+      </Grid.Row>
+      <Grid.Row>
+        <div style={{ height: '500px', width: '100%' }}>
+          <Editor
+            ref={editor}
+            value={{}}
+            ace={ace}
+            mode="code"
+            allowedModes={['code', 'tree']}
+            htmlElementProps={{ style: { height: '100%' } }}
+          />
+        </div>
+      </Grid.Row>
+
+      <Button primary onClick={testAction}>
+        Test
+      </Button>
+      <Button onClick={saveAction}>Save to IPFS</Button>
+      <Grid.Row>
+        <Message style={{ width: '100%' }}>
+          <Message.Header>Result:</Message.Header>
+          <p>{resultContent}</p>
+        </Message>
+      </Grid.Row>
+    </Grid>
+  );
+};
+
+export default Ipfs;
---
+++ src/components/pages/ipfs/IpfsList.tsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Header, Table } from 'semantic-ui-react';
+import { axiosInstance } from '../../../api/api';
+
+interface ApiCall {
+  hashes: string[];
+}
+
+const IpfsList = (): JSX.Element => {
+  const [, setIsLoading] = useState(true);
+  const [ipfsHashes, setIpfsHashes] = useState<string[]>([]);
+
+  useEffect(() => {
+    // Async/Await pattern
+    const update = async (): Promise<void> => {
+      try {
+        setIsLoading(true);
+        const res = await axiosInstance.get<ApiCall>('ipfs');
+        setIpfsHashes(res.data.hashes);
+        setIsLoading(false);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    update();
+  }, []);
+
+  const ipfsItems = ipfsHashes.map((hash) => (
+    <Table.Row>
+      <Table.Cell>
+        <Link to={`/ipfs/${hash}`}>{hash}</Link>
+      </Table.Cell>
+    </Table.Row>
+  ));
+
+  return (
+    <div className="column">
+      <Link to="/ipfs/new">
+        <Button primary>
+          <i className="plus icon" />
+          New PQL definition
+        </Button>
+      </Link>
+
+      <Header as="h1">List of local IPFS hashes</Header>
+
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>IPFS hash</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>{ipfsItems}</Table.Body>
+      </Table>
+    </div>
+  );
+};
+
+export default IpfsList;
