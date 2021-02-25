ea39b072eca2c55e4bc3ef752d0f8bdad19d2761
Žiga Franko <franko.ziga@gmail.com>
2021-02-25 19:13:43 +0100

    urls applied in NavigationBar

--- src/components/layouts/NavigationBar/NavigationBar.tsx
+++ src/components/layouts/NavigationBar/NavigationBar.tsx
@@ -1,35 +1,32 @@
 import React, { useContext } from 'react';
 import { FaUserCircle } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import UserContext from '../../../hooks/UserContext';
+import UserContext from '../../../state/user';
+import { HOME_PAGE, IPFS_BASE_PAGE } from '../../urls';
 import './NavigationBar.scss';
 
 const NavigationBar = (): JSX.Element => {
-  const userContext = useContext(UserContext);
-
-  const logout = (): void => {
-    userContext.logout();
-  };
+  const { user, logout } = useContext(UserContext);
 
   return (
     <div className="navigation-bar">
       <nav className="navigation-bar__nav">
         <div className="navigation-bar__general">
-          <Link className="navigation-bar__logo" to="/">
+          <Link className="navigation-bar__logo" to={HOME_PAGE}>
             <img className="navigation-bar__logo-img" src="/assets/images/logo-icon.png" alt="Paralink Network" />
           </Link>
           <div className="navigation-bar__content">
             <ul className="navigation-bar__links">
               <li className="navigation-bar__link">
-                <Link to="/ipfs">Paralink Network</Link>
+                <Link to={IPFS_BASE_PAGE}>Paralink Network</Link>
               </li>
               <li className="navigation-bar__link">
-                <Link to="/ipfs">IPFS</Link>
+                <Link to={IPFS_BASE_PAGE}>IPFS</Link>
               </li>
             </ul>
           </div>
         </div>
-        {userContext.isLoggedIn ? (
+        {user.isLoggedIn ? (
           <div className="navigation-bar__user">
             {/* TODO: Need to have this as a small menu showing up */}
             <a className="navigation-bar__link navigation-bar__user-link" onClick={logout} href="/login">
