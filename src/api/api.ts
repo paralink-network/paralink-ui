d706210ade4e240746da60be4a7e8991565468a6
Žiga Franko <franko.ziga@gmail.com>
2021-02-25 18:16:47 +0100

    JWT token authentication prepared.
    - Defined access and refresh tokens
    - Prepared interceptor for obtaining new access token or redirecting to the login page when refresh token is expired

---
+++ src/api/api.ts
@@ -0,0 +1,64 @@
+import axios from 'axios';
+
+const OBTAIN_TOKEN = '/token/obtain/';
+// const REFRESH_TOKEN_URL = '/token/refresh/';
+
+export const ACCESS_TOKEN = 'access_token';
+export const REFRESH_TOKEN = 'refresh_token';
+
+interface JWTTokenResponse {
+  refresh: string;
+  access: string;
+}
+
+export const axiosInstance = axios.create({
+  baseURL: 'http://127.0.0.1:7424/api/',
+  timeout: 5000,
+  headers: {
+    // 'Authorization': localStorage.getItem('access_token') ? 'JWT ' + localStorage.getItem('access_token') : null,
+    'Content-Type': 'application/json',
+    accept: 'application/json',
+  },
+});
+
+export const saveTokens = (jwtToken: JWTTokenResponse): JWTTokenResponse => {
+  localStorage.setItem(ACCESS_TOKEN, jwtToken.access);
+  localStorage.setItem(REFRESH_TOKEN, jwtToken.refresh);
+  return jwtToken;
+};
+
+export const getAccessToken = (): string => localStorage.getItem(ACCESS_TOKEN)!;
+export const getRefreshToken = (): string => localStorage.getItem(REFRESH_TOKEN)!;
+
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    // const originalRequest = err.config;
+    // If refresh tokens is expired redirect to login page
+    // if (err.response.status === 401 && originalRequest.url === REFRESH_TOKEN_URL) {
+    //   window.location.href = '/login/';
+    //   return Promise.reject(err);
+    // }
+
+    // // If access token is expired update it
+    // if (err.response.status === 401 && err.response.statusText === 'Unauthorized') {
+    //   return axiosInstance
+    //     .post<JWTTokenResponse>(REFRESH_TOKEN_URL, {refresh: localStorage.getItem(REFRESH_TOKEN)})
+    //     .then(res => res.data)
+    //     .then(saveTokens)
+    //     .then(res => {
+    //       axiosInstance.defaults.headers['Authorization'] = 'JWT ' + res.access;
+    //       originalRequest.headers['Authorization'] = 'JWT ' + res.access;
+
+    //       return axiosInstance(originalRequest);
+    //     })
+    // }
+
+    return Promise.reject(err);
+  },
+);
+
+export const obtainTokenApi = async (email: string, password: string): Promise<JWTTokenResponse> =>
+  axiosInstance
+    .post<JWTTokenResponse>(OBTAIN_TOKEN, { email, password })
+    .then((res) => res.data);
---
+++ src/api/auth/auth.ts
@@ -0,0 +1,24 @@
+// import { axiosInstance, obtainTokenApi, saveTokens } from "../api"
+
+// const LOGIN_URL = '';
+
+export interface UserAuthenticationData {
+  email: string;
+  password: string;
+}
+
+const loginUser = (data: UserAuthenticationData): Promise<void> => {
+  const { email, password } = data;
+
+  if (email !== 'test@test.com') throw new Error('Email does not exist!');
+  if (password !== 'geslo123') throw new Error('Password is incorrect!');
+  // axiosInstance.post<{}>(LOGIN_URL, data)
+  //   .then((res) => res.data);
+  return Promise.resolve();
+};
+
+export const authenticateUser = async (data: UserAuthenticationData): Promise<void> => {
+  await loginUser(data);
+  // const jwtTokens = await obtainTokenApi(data.email, data.password);
+  // saveTokens(jwtTokens);
+};
