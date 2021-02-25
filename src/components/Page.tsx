9f32b910c7c73de4ced5eaddc461d46559de833c
Žiga Franko <franko.ziga@gmail.com>
2021-02-25 19:17:03 +0100

    ProtectedRoute replaced with Page component, which ensures that all his defined routes are protected by checking if user is logged in and if we have access token defined

---
+++ src/components/Page.tsx
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import IpfsList from './pages/ipfs/IpfsList';
+import Ipfs from './pages/ipfs/Ipfs';
+import UserContext from '../state/user';
+import { IPFS_BASE_PAGE, IPFS_PAGE, LOGIN_PAGE } from './urls';
+
+const Page = (): JSX.Element => {
+  const { user } = useContext(UserContext);
+
+  if (!user.isLoggedIn) return <Redirect to={LOGIN_PAGE} />;
+
+  return (
+    <Switch>
+      <Route exact path={IPFS_BASE_PAGE} component={IpfsList} />
+      <Route path={IPFS_PAGE} component={Ipfs} />
+    </Switch>
+  );
+};
+
+export default Page;
