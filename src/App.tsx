633d8fa860a8e95e745b8423a5233d73fb53efa7
Žiga Franko <franko.ziga@gmail.com>
2021-03-02 00:59:37 +0100

    New tailwind NavigationBar, BaseLayout removed and placed into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
-import BaseLayout from './components/layouts/BaseLayout';
-import Login from './components/pages/auth/Login';
-import Page from './components/Page';
-import Error404 from './components/pages/errors/Error404';
 import UserContext, { emptyUser } from './state/user';
-import './App.scss';
-import "tailwindcss/tailwind.css"
+import Page from './components/Page';
+import Login from './components/pages/auth/Login';
+import Error404 from './components/pages/errors/Error404';
+import NavigationBar from './components/navigation/NavigationBar';
 import { ERROR_404_PAGE_ROUTE, LOGIN_PAGE_ROUTE, USER_ROUTES } from './components/routes';
+import "tailwindcss/tailwind.css"
+import './App.scss';
 
 function App(): JSX.Element {
+  // TODO this is a hack! Move all Context providers in some higher component!
+  // TODO keep only Router and BaseLayout here!
+  // Replace window.location.pathname = ... to history.push(...)
   const [user, setUser] = useState({ ...emptyUser });
 
   const login = (email: string): void => setUser({ email, isLoggedIn: true });
-  const logout = (): void => setUser({ ...emptyUser });
+  const logout = (): void => {
+    setUser({ ...emptyUser });
+    window.location.pathname = LOGIN_PAGE_ROUTE;
+  };
 
   return (
-    <UserContext.Provider value={{ user, login, logout }}>
-      <Router>
-        <BaseLayout>
-          <Switch>
-            <Route path={LOGIN_PAGE_ROUTE} component={Login} />
-            <Route path={ERROR_404_PAGE_ROUTE} component={Error404} />
-            <Route path={USER_ROUTES} component={Page} />
-            <Route path="/" render={() => <Redirect to={ERROR_404_PAGE_ROUTE} />} />
-          </Switch>
-        </BaseLayout>
-      </Router>
-    </UserContext.Provider>
+    <Router>
+      <UserContext.Provider value={{ user, login, logout }}>
+        <div className='h-screen flex flex-col'>
+          <NavigationBar />
+          <div className="container-fluid flex-auto">
+            <Switch>
+              <Route path={LOGIN_PAGE_ROUTE} component={Login} />
+              <Route path={ERROR_404_PAGE_ROUTE} component={Error404} />
+              <Route path={USER_ROUTES} component={Page} />
+              <Route path="/" render={() => <Redirect to={ERROR_404_PAGE_ROUTE} />} />
+            </Switch>
+          </div>
+        </div>
+      </UserContext.Provider>
+    </Router>
   );
 }
 
---
+++ src/components/navigation/NavigationBar.tsx
@@ -0,0 +1,75 @@
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import UserContext from '../../state/user';
+import { HOME_PAGE_ROUTE, IPFS_BASE_PAGE_ROUTE, QUERY_LIST_ROUTE } from '../routes';
+
+const NavigationBar = (): JSX.Element => {
+  const { logout } = useContext(UserContext);
+
+  const [userShow, setUserShow] = useState(false);
+  const [mobileShow, setMobileShow] = useState(false);
+
+  const userShowToggle = () => setUserShow(!userShow);
+  const mobileShowToggle = () => setMobileShow(!mobileShow);
+
+  return (
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false" onClick={mobileShowToggle}>
+              <span className="sr-only">Open main menu</span>
+              <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+              <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </div>
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex-shrink-0 flex items-center">
+              <img className="block lg:hidden h-8 w-auto" src="/assets/images/logo-icon.png" alt="Workflow" />
+              <img className="hidden lg:block h-8 w-auto" src="/assets/images/logo-icon.png" alt="Workflow" />
+            </div>
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex space-x-4">
+                <Link to={HOME_PAGE_ROUTE} className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</Link>
+                <Link to={IPFS_BASE_PAGE_ROUTE} className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Paralink Network</Link>
+                <Link to={QUERY_LIST_ROUTE} className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Query list</Link>
+                <Link to={QUERY_LIST_ROUTE + '/new'} className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Query</Link>
+              </div>
+            </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div className="ml-3 relative">
+              <div>
+                <button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-haspopup="true" aria-expanded="false" onClick={userShowToggle}>
+                  <span className="sr-only">Open user menu</span>
+                  <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                </button>
+              </div>
+              <div className={`origin-top-right absolute right-0 mt-2 py-1 z-10 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 ${userShow ? "show" : "hidden"}`} role="menu" aria-orientation="vertical">
+                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your Profile</a>
+                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</a>
+                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" onClick={logout}>Sign out</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={`sm:hidden ${mobileShow ? "show" : "hidden"}`} aria-label="mobile-menu">
+        <div className="px-2 pt-2 pb-3 space-y-1">
+
+          <Link to={HOME_PAGE_ROUTE} className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Home</Link>
+          <Link to={IPFS_BASE_PAGE_ROUTE} className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Paralink Network</Link>
+          <Link to={QUERY_LIST_ROUTE} className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Query list</Link>
+          <Link to={QUERY_LIST_ROUTE + '/new'} className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Query</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavigationBar;
