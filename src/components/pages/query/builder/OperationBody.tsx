776ebb188439045817e30eca3a61b90114473576
Žiga Franko <franko.ziga@gmail.com>
2021-03-04 18:44:44 +0100

    Operation configuration moved out of PqlOperation

---
+++ src/components/pages/query/builder/OperationBody.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { InlineIcon } from '@iconify/react';
+import cogIcon from '@iconify-icons/mdi/cog';
+import minusIcon from '@iconify-icons/mdi/minus';
+import arrowRightDropCircleOutline from '@iconify-icons/mdi/arrow-right-drop-circle-outline';
+import { TooltipButton } from '../../../common/Buttons';
+
+interface OperationBody {
+  onRun: () => void;
+  onConfig: () => void;
+  onRemove: () => void;
+}
+
+const OperationBody = ({onRemove, onConfig, onRun}: OperationBody) => (
+  <div className="flex flex-row">
+    <TooltipButton className="rounded-full ml-1" color="green" tooltip="Run" onClick={onRun}>
+      <InlineIcon icon={arrowRightDropCircleOutline} />
+    </TooltipButton>
+    <TooltipButton className="rounded-full ml-1" tooltip="Config" onClick={onConfig}>
+      <InlineIcon icon={cogIcon} />
+    </TooltipButton>
+    <TooltipButton className="rounded-full ml-1" color="red" tooltip="Remove" onClick={onRemove}>
+      <InlineIcon icon={minusIcon} className="m-auto" />
+    </TooltipButton>
+  </div>
+);
+
+export default OperationBody;
--- src/components/pages/query/builder/PqlOperation.tsx
+++ src/components/pages/query/builder/PqlOperation.tsx
@@ -1,38 +1,27 @@
 import React from 'react';
-import { InlineIcon } from '@iconify/react';
-import cogIcon from '@iconify-icons/mdi/cog';
-import minusIcon from '@iconify-icons/mdi/minus';
-import arrowRightDropCircleOutline from '@iconify-icons/mdi/arrow-right-drop-circle-outline';
 import { Draggable } from 'react-beautiful-dnd';
 import { ExtendedOperator } from './builder';
-import { TooltipButton } from '../../../common/Buttons';
+import { OperatorKind } from '../../../../state/pql/pql';
+import OperationBody from './OperationBody';
 
 interface PqlOperation extends ExtendedOperator {
   index: number;
+  onRun: (id: string) => void;
   onConfigClick: (id: string) => void;
+  onRemove: (id: string) => void;
 }
 
-const PqlOperation = ({ id, index, operator, onConfigClick }: PqlOperation): JSX.Element => (
-  <Draggable draggableId={id} index={index}>
-    {(provided) => (
+const PqlOperation = ({ id, index, operator, onConfigClick, onRemove, onRun }: PqlOperation): JSX.Element => (
+  <Draggable draggableId={id} index={index} isDragDisabled={operator.kind === OperatorKind.Loader}>
+    {(provided, snapshot) => (
       <div
-        className="bg-white border rounded px-5 py-2 mb-1 flex flex-row justify-between"
         {...provided.draggableProps}
         {...provided.dragHandleProps}
+        className={`border rounded px-5 py-2 mb-1 flex flex-row justify-between ${snapshot.isDragging ? 'bg-blue-100' : 'bg-white'}`}
         ref={provided.innerRef}
       >
         {operator.title}
-        <div className="flex flex-row">
-          <TooltipButton className="rounded-full ml-1" color="green" tooltip="Run">
-            <InlineIcon icon={arrowRightDropCircleOutline} />
-          </TooltipButton>
-          <TooltipButton className="rounded-full ml-1" tooltip="Config" onClick={() => onConfigClick(id)}>
-            <InlineIcon icon={cogIcon} />
-          </TooltipButton>
-          <TooltipButton className="rounded-full ml-1" color="red" tooltip="Remove">
-            <InlineIcon icon={minusIcon} className="m-auto" />
-          </TooltipButton>
-        </div>
+        <OperationBody onRun={() => onRun(id)} onConfig={() => onConfigClick(id)} onRemove={() => onRemove(id)} />
       </div>
     )}
   </Draggable>
