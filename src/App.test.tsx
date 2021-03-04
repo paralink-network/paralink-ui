21c6a9ad05853a10408770bbd0793e537d1e115f
Žiga Franko <franko.ziga@gmail.com>
2021-03-04 19:00:01 +0100

    App test updated to search for login screen

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,7 +5,7 @@ import App from './App';
 describe('App', () => {
   test('renders the page', () => {
     render(<App />);
-    const linkElement = screen.getByText(/Paralink Network/i);
+    const linkElement = screen.getByText('Login into your account');
     expect(linkElement).toBeInTheDocument();
   });
 });
