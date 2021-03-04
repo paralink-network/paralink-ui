edaf05e07f0b31419d19e0f8b737bb9d0d83541f
Žiga Franko <franko.ziga@gmail.com>
2021-03-04 18:46:27 +0100

    Closable container has additional Submit button which creates the same action as Back

--- src/components/pages/query/QueryClosableContainer.tsx
+++ src/components/pages/query/QueryClosableContainer.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { InlineIcon } from '@iconify/react';
 import arrowLeft from '@iconify-icons/mdi/arrow-left';
 import { TooltipButton } from '../../common/Buttons';
+import { Button } from 'semantic-ui-react';
 
 interface QueryClosableContainer {
   onClose: () => void;
+  submitButton?: boolean;
 }
 
-const QueryClosableContainer: React.FC<QueryClosableContainer> = ({ onClose, children }) => (
+const QueryClosableContainer: React.FC<QueryClosableContainer> = ({ onClose, submitButton=false, children }) => (
   <div className="flex flex-col">
     <div className="mb-3">
       <TooltipButton className="rounded-full" onClick={onClose} tooltip="Back">
@@ -15,6 +17,11 @@ const QueryClosableContainer: React.FC<QueryClosableContainer> = ({ onClose, chi
       </TooltipButton>
     </div>
     {children}
+    {submitButton && 
+      <div className='flex justify-center mt-3'>
+        <Button color="green" onClick={onClose}>Submit</Button>
+      </div>
+    }
   </div>
 );
 
