2351c7f200423c4bb09ef0cefdbec9b22820acce
Žiga Franko <franko.ziga@gmail.com>
2021-03-04 14:24:17 +0100

    OperationConfig refresh callback updated

--- src/components/pages/query/OperationConfig.tsx
+++ src/components/pages/query/OperationConfig.tsx
@@ -10,10 +10,13 @@ const OperationConfig = ({ onClose, operator }: OperationConfig): JSX.Element =>
   // Because our operators are not common components, when their state is internally updated
   // we need to trigger this (parent) component to refresh childs component view.
   const [refreshIndex, setRefreshIndex] = useState(true);
-  const refreshCallback = (): void => setRefreshIndex(!refreshIndex);
+  const refresh = <T,>(fun: (value: T) => void) => (value: T) => {
+    fun(value);
+    setRefreshIndex(!refreshIndex);
+  }
   useEffect(() => {}, [refreshIndex]);
 
-  return <QueryClosableContainer onClose={onClose}>{operator.renderConfig(refreshCallback)}</QueryClosableContainer>;
+  return <QueryClosableContainer onClose={onClose}>{operator.renderConfig(refresh)}</QueryClosableContainer>;
 };
 
 export default OperationConfig;
