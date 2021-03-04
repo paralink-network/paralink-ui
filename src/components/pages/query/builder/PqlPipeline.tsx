1051173a615499ffd3f48ea1b228367055f06e9b
Žiga Franko <franko.ziga@gmail.com>
2021-03-04 18:45:26 +0100

    PqlPipeline now has the ability to change order or Operators

--- src/components/pages/query/builder/PqlPipeline.tsx
+++ src/components/pages/query/builder/PqlPipeline.tsx
@@ -1,14 +1,67 @@
 import React, { useEffect, useState } from 'react';
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
+import { isConstructorDeclaration } from 'typescript';
+import { OperatorKind } from '../../../../state/pql/pql';
+import { onOperatorRemoveAction } from '../../../../state/query-builder';
 import { QueryData } from './builder';
+import OperationBody from './OperationBody';
 import PqlSource from './PqlSource';
+
+type UpdateData = (data: QueryData) => void;
+
+// This function needs to be take care off...
+// Too much if statements
+const onDragEnd = (data: QueryData, setData: UpdateData) => (result: DropResult) => {
+  const { destination, source, draggableId } = result;
+
+  if (!destination) return;
+  if (source.droppableId === destination.droppableId && source.index === destination.index) return;
+
+  if (destination.droppableId === source.droppableId) {
+    const column = data.sources[source.droppableId];
+    const newOperators = [...column.operators];
+    newOperators.splice(source.index, 1);
+    newOperators.splice(destination.index, 0, draggableId);
+
+    setData({...data,
+      sources: {...data.sources,
+        [column.id]: {...column,
+          operators: newOperators
+        }
+      }
+    });
+  }
+  else {
+    const fromColumn = data.sources[source.droppableId];
+    const fromOperators = [...fromColumn.operators];
+    fromOperators.splice(source.index, 1);
+
+    const toColumn = data.sources[destination.droppableId];
+    const toOperators = [...toColumn.operators];
+    toOperators.splice(destination.index, 0, draggableId);
+
+    setData({...data,
+      sources: {...data.sources,
+        [fromColumn.id]: {...fromColumn,
+          operators: [...fromOperators],
+        },
+        [toColumn.id]: {...toColumn,
+          operators: [...toOperators]
+        }
+      }
+    });
+  }
+}
+
+
 
 interface PqlPipeline {
   data: QueryData;
+  setData: UpdateData;
   onConfigClick: (id: string) => void;
 }
 
-const PqlPipeline = ({ data, onConfigClick }: PqlPipeline): JSX.Element => {
+const PqlPipeline = ({ data, setData, onConfigClick }: PqlPipeline): JSX.Element => {
   const [change, setChange] = useState(false);
   const update = (): void => setChange(!change);
   useEffect((): void => {}, [change]);
@@ -21,6 +74,8 @@ const PqlPipeline = ({ data, onConfigClick }: PqlPipeline): JSX.Element => {
       data.sources[sourceId].title = value;
       update();
     };
+    const onRemove = (id: string) => 
+      setData(onOperatorRemoveAction(data, sourceId, id));
 
     return (
       <PqlSource
@@ -29,6 +84,8 @@ const PqlPipeline = ({ data, onConfigClick }: PqlPipeline): JSX.Element => {
         title={title}
         onTitleChange={onTitleChange}
         operators={operationItems}
+        onRun={() => {}}
+        onRemove={onRemove}
         onConfigClick={onConfigClick}
       />
     );
@@ -36,10 +93,13 @@ const PqlPipeline = ({ data, onConfigClick }: PqlPipeline): JSX.Element => {
 
   return (
     <div>
-      <DragDropContext onDragEnd={() => {}}>{sourcesView}</DragDropContext>
-      <div className="pb-2 px-1">
-        <div className="border rounded px-5 py-2 mb-1">Aggregare</div>
-      </div>
+      <DragDropContext onDragEnd={onDragEnd(data, setData)}>{sourcesView}</DragDropContext>
+      {data.aggregate && 
+        <div className={`border rounded px-5 py-2 mb-1 flex flex-row justify-between`}>
+          Aggregare
+          <OperationBody onRun={() => {}} onRemove={() => {}} onConfig={() => {}} />
+        </div>
+      }
     </div>
   );
 };
