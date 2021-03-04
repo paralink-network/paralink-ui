0fdbbe210cb2cf708c1f616f03ed08c1ce643419
Žiga Franko <franko.ziga@gmail.com>
2021-03-04 14:26:19 +0100

    QueryLoader changed from Promise pattern to Async/Await and added Pql state

--- src/components/pages/query/QueryLoader.tsx
+++ src/components/pages/query/QueryLoader.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { loadIPFSWithHash } from '../../../api/pql';
+import { Pql, emptyPql } from '../../../state/pql/pql';
 import ErrorContainer from '../../common/sub-pages/ErrorContainer';
 import Loading from '../../common/sub-pages/Loading';
 import { convertPql, emptyQueryData } from './builder/builder';
-import QueryController from './QueryContainer';
+import QueryController from './QueryController';
 
 interface UrlParams {
   hash: string;
@@ -14,21 +15,31 @@ const QueryLoader = (): JSX.Element => {
   const { hash } = useParams<UrlParams>();
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState('');
+  const [pql, setPql] = useState<Pql>({...emptyPql});
   const [queryData, setQueryData] = useState({ ...emptyQueryData });
 
   useEffect(() => {
-    Promise.resolve()
-      .then(() => setIsLoading(true))
-      .then(() => loadIPFSWithHash(hash))
-      .then((res) => convertPql(res.pql))
-      .then(setQueryData)
-      .catch((err) => setError(err.message))
-      .finally(() => setIsLoading(false));
+    const load = async () => {
+      try {
+        setIsLoading(true);
+        const res = await loadIPFSWithHash(hash);
+        const data = convertPql(res.pql);
+        setPql(res.pql);
+        setQueryData(data);
+      } 
+      catch(error) {
+        setError(error.message);
+      }
+      finally {
+        setIsLoading(false);
+      }
+    }
+    load();
   }, [hash]);
 
   if (isLoading) return <Loading />;
   if (error.length > 0) return <ErrorContainer message={error} hash={hash} />;
-  return <QueryController queryData={queryData} />;
+  return <QueryController queryData={queryData} pql={pql} />;
 };
 
 export default QueryLoader;
