5f45a58864bcf249ac6e4b45e42bb3f451777c80
Žiga Franko <franko.ziga@gmail.com>
2021-03-11 11:10:14 +0100

    partial compile defined

--- src/components/pages/query/builder/compile.ts
+++ src/components/pages/query/builder/compile.ts
@@ -13,3 +13,37 @@ export const compile = (name: string, pqlVersion: string, data: QueryData): Pql 
     })),
     aggregate: data.aggregate ? data.aggregate.build() : undefined,
 });
+
+export const partialCompile = (name: string, pqlVersion: string, data: QueryData, endSourceId: string, endOperatorId: string): Pql => {
+  const endSourceIndex = data.sourceOrder
+    .findIndex((source) => source === endSourceId);
+
+  const sources = data.sourceOrder
+    .filter((item, index) => index <= endSourceIndex)
+    .map((sourceId) => {
+      const source = data.sources[sourceId];
+      let endOperatorIndex = source.operators
+        .findIndex((operator) => operator === endOperatorId);
+      
+      // If the end operator is not in the current source, find index will return -1, 
+      // therefore, we need to update it to the full length of operators
+      // because of the next filter method
+      if (endOperatorIndex === -1)
+        endOperatorIndex = source.operators.length;
+      
+      const operations = source.operators
+        .filter((operator, index) => index <= endOperatorIndex)
+        .map((operator) => data.operators[operator].operator.build());
+      
+      return {
+        name: source.title,
+        pipeline: operations,
+      };
+    })
+
+  return {
+    name,
+    sources,
+    psql_version: pqlVersion,
+  }
+};
