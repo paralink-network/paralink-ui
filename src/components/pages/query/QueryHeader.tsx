3be83e3834f621c351e4c780817f272abefbe548
Žiga Franko <franko.ziga@gmail.com>
2021-03-11 10:12:54 +0100

    QueryHeader now includes all actions

--- src/components/pages/query/QueryHeader.tsx
+++ src/components/pages/query/QueryHeader.tsx
@@ -4,15 +4,24 @@ import arrowRightDropCircleOutline from '@iconify-icons/mdi/arrow-right-drop-cir
 import hammerIcon from '@iconify-icons/mdi/hammer';
 import contentSaveOutline from '@iconify-icons/mdi/content-save-outline';
 import reloadIcon from '@iconify-icons/mdi/reload';
+import codeJson from '@iconify-icons/mdi/code-json';
+import clipboardTextOutline from '@iconify-icons/mdi/clipboard-text-outline';
 import { TooltipButton } from '../../common/Buttons';
 import { LabelInput } from '../../common/Inputs';
 
 interface QueryHeader {
+  showResult: boolean;
   projectName: string;
+
+  onRun: () => void;
+  onSave: () => void;
+  onBuild: () => void;
+  onCompile: () => void;
+  onResultCodeSwitch: () => void;
   setProjectName: (value: string) => void;
 }
 
-const QueryHeader = ({ projectName, setProjectName }: QueryHeader): JSX.Element => (
+const QueryHeader = ({ projectName, setProjectName, showResult, onResultCodeSwitch, onRun, onSave, onBuild, onCompile }: QueryHeader): JSX.Element => (
   <div className="p-2 h-50 shadow-sm flex flex-column justify-between">
     <LabelInput
       value={projectName}
@@ -23,16 +32,19 @@ const QueryHeader = ({ projectName, setProjectName }: QueryHeader): JSX.Element 
     />
 
     <div className="mt-1 mb-1">
-      <TooltipButton onClick={() => {}} className="ml-1" tooltip="Save">
+      <TooltipButton onClick={onResultCodeSwitch} className="ml-1" tooltip={showResult ? "Show code" : "Show result"}>
+        <Icon icon={showResult ? codeJson : clipboardTextOutline} width="20" />
+      </TooltipButton>
+      <TooltipButton onClick={onSave} className="ml-1" tooltip="Save">
         <Icon icon={contentSaveOutline} width="20" />
       </TooltipButton>
-      <TooltipButton onClick={() => {}} className="ml-1" tooltip="Reload">
-        <Icon icon={reloadIcon} width="20" />
-      </TooltipButton>
-      <TooltipButton onClick={() => {}} className="ml-1" tooltip="Build">
+      <TooltipButton onClick={onBuild} className="ml-1" tooltip="Build">
         <Icon icon={hammerIcon} width="20" />
       </TooltipButton>
-      <TooltipButton onClick={() => {}} color="green" className="ml-1" tooltip="Run">
+      <TooltipButton onClick={onCompile} className="ml-1" tooltip="Compile">
+        <Icon icon={reloadIcon} width="20" />
+      </TooltipButton>
+      <TooltipButton onClick={onRun} color="green" className="ml-1" tooltip="Run">
         <Icon icon={arrowRightDropCircleOutline} width="20" />
       </TooltipButton>
     </div>
