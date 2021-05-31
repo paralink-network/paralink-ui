d68e200f7e5620062bcbaa01651a614f08616bf3
Žiga Franko <franko.ziga@gmail.com>
2021-05-31 15:39:41 +0200

    cards have on click method and styling updated

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -1,17 +1,17 @@
 import React from "react"
 
 interface CardProps {
-
+  onClick?: () => void;
 }
 
-export const Card: React.FC<CardProps> = ({children}) => (
-  <div className="bg-white px-4 py-5 border border-gray-200 sm:px-6">
+export const Card: React.FC<CardProps> = ({children, onClick}) => (
+  <div className="bg-white px-4 py-5 border border-gray-200 sm:px-6" onClick={onClick}>
     {children}
   </div>
 );
 
-export const HoverCard: React.FC<CardProps> = ({children}) => (
-  <div className="bg-white px-4 py-5 border border-gray-200 sm:px-6 hover:bg-green-500 cursor-pointer">
+export const HoverCard: React.FC<CardProps> = ({children, onClick}) => (
+  <div className="bg-white px-4 py-5 border border-gray-200 sm:px-6 hover:bg-blue-600 hover:text-gray-200 cursor-pointer" onClick={onClick}>
     {children}
   </div>
 );
